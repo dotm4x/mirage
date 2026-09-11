8a7eaa05ee75302147e85b723f8161ba1e8d7acd
Max <max@stonebogus.com>
2026-09-10 21:55:13 -0600

    docs: add XML documentation

--- Mirage/Mirage.Core/Exceptions/DestroyedObject.cs
+++ Mirage/Mirage.Core/Exceptions/DestroyedObject.cs
@@ -1,3 +1,7 @@
 namespace Mirage.Core.Exceptions;
 
+/// <summary>
+/// Represents an exception thrown when an operation is attempted on an object
+/// that has already been destroyed.
+/// </summary>
 public class DestroyedObjectException(string message) : InvalidOperationException(message) { }
