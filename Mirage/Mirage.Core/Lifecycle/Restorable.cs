3f3dbfeccc2e6bbab035695dc3e2f307a929dd58
Max <max@stonebogus.com>
2026-09-14 15:01:53 -0600

    add: create IRestorable interface

---
+++ Mirage/Mirage.Core/Lifecycle/Restorable.cs
@@ -0,0 +1,12 @@
+namespace Mirage.Core.Lifecycle;
+
+/// <summary>
+/// Defines an object that can restore its state.
+/// </summary>
+public interface IRestorable
+{
+    /// <summary>
+    /// Restores the object to its initial state.
+    /// </summary>
+    void Restore();
+}
