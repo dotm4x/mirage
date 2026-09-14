9515a1ee70273dbb499ed38ee2eed65862ce5f7f
Max <max@stonebogus.com>
2026-09-14 14:44:25 -0600

    documentation: add XML documentation

--- Mirage/Mirage.Core/Resource.cs
+++ Mirage/Mirage.Core/Resource.cs
@@ -2,7 +2,12 @@ using Mirage.Core.Lifecycle;
 
 namespace Mirage.Core;
 
+/// <summary>
+/// Represents reusable data that can be shared between objects.
+/// </summary>
+/// <remarks>
+/// A resource can be destroyed when it is no longer needed.
+/// </remarks>
 public abstract class Resource : Destroyable
 {
-    
 }
