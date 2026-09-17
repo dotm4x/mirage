41a2a06414af0b926c222db1e6e875dda25a9c15
Max <max@stonebogus.com>
2026-09-16 23:39:10 -0600

    fix: correct syntax error

--- Engine/Graph/Graph.cs
+++ Engine/Graph/Graph.cs
@@ -1,6 +1,34 @@
+using Mirage.Common;
+using Mirage.Common.Collections;
+
 namespace Mirage.Graph;
 
-public class Graph
+public class Graph : Module
 {
-    public readonly
+    private readonly ReactiveDictionary<string, Node> _roots = [];
+    public readonly IReadOnlyReactiveDictionary<string, Node> Roots;
+
+    public Graph(IEnumerable<Node> roots)
+        : base("Graph")
+    {
+        Roots = _roots;
+
+        foreach (var root in roots)
+            _roots.Add(root.Identifier.ToString(), root);
+
+        _roots.OnAdd.Connect(
+            (root) =>
+            {
+                OnRootAdded(root.Value);
+            }
+        );
+    }
+
+    private void OnRootAdded(Node node)
+    {
+        if (node.Parent.Get() is not null)
+            throw new InvalidOperationException(
+                $"Cannot add {node} as a graph root because it already has a parent."
+            );
+    }
 }
